247d900de0fc198c3f02241956551e1d32d86dbe
ILPillow2607 <rachatanine9@gmail.com>
2026-04-25 21:20:38 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+x = int(input("Give me the first number: "))
+y = int(input("GIve me the second number: "))
+print("Thank you!")
+print(x, "+", y, "=", x + y)
+print(x, "-", y, "=", x - y)
+print(x, "/", y, "=", x / y)
+print(x, "*", y, "=", x * y)
