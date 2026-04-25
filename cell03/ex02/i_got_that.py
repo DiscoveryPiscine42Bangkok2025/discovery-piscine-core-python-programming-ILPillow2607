ee81c89279219af34b2be7f3140b20407e4c80dd
ILPillow2607 <rachatanine9@gmail.com>
2026-04-25 19:29:58 +0700

    Create i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+user_input = input("What you gonna say? : ")
+
+while user_input != "STOP":
+    user_input = input(f"I got that! Anything else? : {user_input} \n")
+
+print("I got that! Anything else? : STOP")
