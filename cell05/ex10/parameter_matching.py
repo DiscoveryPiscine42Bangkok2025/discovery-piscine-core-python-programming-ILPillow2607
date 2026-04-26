85de078cb4b85de73ba43ad76bf8a3f4abdb12a6
ILPillow2607 <rachatanine9@gmail.com>
2026-04-26 20:28:38 +0700

    Create parameter_matching.py

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+    param = sys.argv[1]
+    user_input = input("What was the parameter? ")
+    if user_input == param:
+        print("Good job!")
+    else:
+        print("Nope sorry...")
+
+if __name__ == "__main__":
+    main()
+#python parameter_matching.py "Hello" (for try this code)
